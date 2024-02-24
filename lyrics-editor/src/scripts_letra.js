7cfee5d5c65450aa453b45c51920c63c904f0277
Frialdad <161152944+Frialdad@users.noreply.github.com>
2024-02-24 19:59:14 +0100

    Add files via upload

--- lyrics-editor/src/scripts_letra.js
+++ lyrics-editor/src/scripts_letra.js
@@ -1,62 +1,89 @@
 function checkText() {
-    var inputText = document.getElementById("input").value;
+  var inputText = document.getElementById("input").value;
 
-    // Translation map
-    var translationMap = {
-        "Instrumental": "Instrumental",
-        "Intro": "Intro",
-        "Verse": "Verso",
-        "Pre-Chorus": "Pre-Coro",
-        "Chorus": "Coro",  
-        "Post-Chorus": "Post-Coro",
-        "Refrain": "Refrán",
-        "Bridge": "Puente",
-        "Interlude": "Interludio",
-        "Spoken": "Hablado",
-        "Breakdown": "Ruptura",
-        "Outro": "Outro",
-        "Build": "Build",
-        "Drop": "Drop"
-    };
+  // Translation map
+  var translationMap = {
+    "Instrumental": "Instrumental",
+    "Intro": "Intro",
+    "Verse": "Verso",
+    "Pre-Chorus": "Pre-Coro",
+    "Chorus": "Coro",
+    "Post-Chorus": "Post-Coro",
+    "Refrain": "Refrán",
+    "Bridge": "Puente",
+    "Interlude": "Interludio",
+    "Spoken": "Hablado",
+    "Breakdown": "Ruptura",
+    "Outro": "Outro",
+    "Build": "Build",
+    "Drop": "Drop",
+    "Instrumental Interlude": "Interludio Instrumental",
+    "Instrumental Breakdown": "Ruptura Instrumental",
+    "Instrumental Bridge": "Puente Instrumental",
+    "Estrofa": "Verso",
+  
+  };
 
-    // Check if the text starts with "[Letra de"
-    if (!inputText.startsWith("[Letra de")) {
-        inputText = "[Letra de \"Título\"]\n\n" + inputText;
+  // Check if the text starts with "[Letra de"
+  if (!inputText.startsWith("[Letra de")) {
+      inputText = "[Letra de \"Título\"]\n\n" + inputText;
+  }
+
+  var lines = inputText.trim().split('\n');
+  var outputText = lines.map(function(verse, index) {
+    verse = verse.replace(/&[^;]*?;/gm, '');
+    verse = verse.replace(/[.,;:!?]$/g, '');
+
+    if (/[а-яА-Я]/.test(verse)) {
+      return "Cyrillic character found";
+    }
+    // Replace different quotes with single quotes
+    verse = verse.replace(/[’´]/g, "'");
+    verse = capitalizeFirstLetter(verse);
+
+    if (verse.startsWith("[")) {
+      // Use a regular expression to match the tag even if it has one or more words
+      var tagMatch = verse.match(/\[([^\]]+)\]/);
+      if (tagMatch) {
+        var x = tagMatch[1].split(":");
+        var tag = x[0].trim(); // Tomar solo la primera palabra antes de ":"
+        if (translationMap[tag]) {
+          if (x.length > 1) {
+            // Reemplazar coma por "&" si hay contenido después de los dos puntos
+            var content = x.slice(1).join(':').trim().replace(',', ' &');
+            verse = "[" + translationMap[tag] + ": " + content + "]";
+          } else {
+            verse = "[" + translationMap[tag] + "]";
+          }
+        } else {
+          if (x.length > 1) {
+            // Reemplazar coma por "&" si hay contenido después de los dos puntos
+            var content = x.slice(1).join(':').trim().replace(',', ' &');
+            verse = "[" + tag + ": " + content + "]";
+          }
+        }
+      }
     }
 
-    var lines = inputText.trim().split('\n');
-    var outputText = lines.map(function (verse, index) {
-        verse = verse.replace(/<[^>]*>?/gm, '');
-        verse = verse.replace(/&[^;]*?;/gm, '');
-        verse = verse.replace(/[.,;:!?]$/g, '');
-        if (/[а-яА-Я]/.test(verse)) {
-            return "Cyrillic character found";
-        }
-        // Replace different quotes with single quotes
-        verse = verse.replace(/[’´]/g, "'");
-        verse = capitalizeFirstLetter(verse);
+    // Apply formatting for <i></i>
+    verse = verse.replace(/<i>(.*?)<\/i>/g, '$1');
 
-        // Check if the verse is a tag that needs to be translated
-        if (verse.startsWith("[")) {
-            var tag = verse.slice(1, verse.indexOf("]"));
-            if (translationMap[tag]) {
-                verse = verse.replace("[" + tag + "]", "[" + translationMap[tag] + "]");
-                // Add a new line after the header only if the next line is not empty
-          
-            }
-        }
+    // Apply formatting for <b></b>
+    verse = verse.replace(/<b>(.*?)<\/b>/g, '$1');
 
-        return verse;
-    }).join('\n');
 
-    var outputElement = document.getElementById("output");
-    outputElement.innerHTML = outputText;
+    return verse;
+  }).join('\n');
+
+  var outputElement = document.getElementById("output");
+  outputElement.textContent = outputText;
 }
 
 function copyText() {
+  try {
     var inputText = document.querySelector("#output").textContent;
-    var outputText = inputText.trim().split('\n').map(function (verse) {
-        return verse;
+    var outputText = inputText.trim().split('\n').map(function(verse) {
+      return verse;
     }).join('\n');
     var tempTextarea = document.createElement('textarea');
     tempTextarea.value = outputText;
@@ -65,14 +92,19 @@ function copyText() {
     document.execCommand('copy');
     document.body.removeChild(tempTextarea);
 
-    alert("Text copied to clipboard.");
+    alert("Letra copiada al portapapeles.");
+  } catch (error) {
+    console.error("Error al copiar el texto:", error);
+    alert("Se produjo un error al copiar el texto. Consulta la consola para más detalles.");
+  }
 }
 
+
 function clearText() {
-    document.getElementById("input").value = "";
-    document.getElementById("output").innerHTML = "";
+  document.getElementById("input").value = "";
+  document.getElementById("output").innerHTML = "";
 }
 
 function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
